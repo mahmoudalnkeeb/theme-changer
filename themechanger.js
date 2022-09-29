92bc96f2d6d20fe1a0dbd99200d0f486c128c905
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2022-09-29 08:10:11 +0200

    upload

---
+++ themechanger.js
@@ -0,0 +1,67 @@
+function createOverlay(overlayStyle, btns, target) {
+  target.style.position = 'relative';
+  let overlay = document.createElement('div');
+  let styles = overlayStyle
+  overlay.setAttribute('style', styles);
+  btns.forEach((btn) => overlay.appendChild(btn));
+  return overlay;
+}
+
+function createBtns(themes , style) {
+  let btns = [];
+  themes.forEach((theme) => {
+    let button = document.createElement('button');
+    button.classList.add('theme-changer');
+    button.setAttribute('data-change', theme.themeName);
+    button.innerText = theme.themeName;
+    button.style.background = theme.btnBg;
+    button.style.color = theme.btnClr;
+    btns.push(button);
+  });
+  style(themes)
+  return btns;
+}
+
+function changeOnClick(target, btns) {
+  btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      let theme = btn.dataset.change;
+      target.dataset.theme = theme;
+    });
+  });
+  return btns;
+}
+
+function appendVarsToStyle(themes) {
+  let style = document.createElement('style');
+  let innerText;
+  themes.forEach((theme) => {
+    innerText = '';
+    innerText = `[data-theme='${theme.themeName}']{
+        `;
+    let keys = Object.keys(theme.themeVars);
+    keys.forEach((key) => {
+      innerText += `${key}:${theme.themeVars[key]};
+      `;
+    });
+    innerText += `}
+    `;
+    style.innerHTML += innerText;
+  });
+  document.head.append(style);
+}
+
+/**
+ *
+ * @param {themes}  themes
+ * @param {overlayStyle}  overlayStyles
+ * 
+ */
+
+function themeChanger(themes, overlayStyles) {
+  let overlay = createOverlay;
+  let evTobtns = changeOnClick;
+  let btns = createBtns;
+  let style = appendVarsToStyle
+  return (x) => overlay(overlayStyles, evTobtns(x, btns(themes , style)), x);
+}
